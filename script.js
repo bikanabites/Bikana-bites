d59122e042642be9abdaab507c1618ce78026353
bikanabites <bikanabites@gmail.com>
2026-06-15 21:00:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,91 @@
-// BIKANA BITES Website JavaScript
+setTimeout(()=>{
+document.getElementById("loader").style.display="none";
+},2000);
 
-document.addEventListener("DOMContentLoaded", function () {
+let cart=[];
+let total=0;
 
-    // Smooth active navigation effect
-    const navLinks = document.querySelectorAll(".nav-links a");
+function changeQty(btn,value){
 
-    navLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            navLinks.forEach(item => item.classList.remove("active"));
-            link.classList.add("active");
-        });
-    });
+let qtyElement=btn.parentElement.querySelector("span");
 
-    // Simple fade-in animation on scroll
-    const elements = document.querySelectorAll(
-        ".product-card, .feature-box, .about-text"
-    );
+let qty=parseInt(qtyElement.innerText);
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if(entry.isIntersecting){
-                entry.target.style.opacity = "1";
-                entry.target.style.transform = "translateY(0)";
-            }
-        });
-    }, {
-        threshold: 0.15
-    });
+qty+=value;
 
-    elements.forEach(element => {
-        element.style.opacity = "0";
-        element.style.transform = "translateY(30px)";
-        element.style.transition = "all 0.8s ease";
-        observer.observe(element);
-    });
+if(qty<1) qty=1;
+
+qtyElement.innerText=qty;
+}
+
+function addToCart(name,price,btn){
+
+let qty=parseInt(
+btn.parentElement.querySelector(".qty-box span").innerText
+);
+
+cart.push({
+name:name,
+price:price,
+qty:qty
+});
+
+total+=price*qty;
+
+document.getElementById("cartCount").innerText=cart.length;
+
+document.getElementById("cartTotal").innerText=total;
+
+alert(name+" Added To Cart");
+}
+
+function checkoutWhatsapp(){
+
+if(cart.length===0){
+
+alert("Cart Empty");
+
+return;
+}
+
+let message="🛒 BIKANA BITES Order%0A%0A";
+
+cart.forEach(item=>{
+
+message +=
+item.name+
+" x "+
+item.qty+
+" = ₹"+
+(item.price*item.qty)+
+"%0A";
 
 });
+
+message += "%0A Total = ₹"+total;
+
+window.open(
+"https://wa.me/919358500110?text="+message,
+"_blank"
+);
+}
+
+function searchProducts(){
+
+let input=document.getElementById("searchInput");
+
+let filter=input.value.toLowerCase();
+
+let cards=document.querySelectorAll(".product-card");
+
+cards.forEach(card=>{
+
+let text=card.innerText.toLowerCase();
+
+if(text.includes(filter))
+card.style.display="block";
+else
+card.style.display="none";
+
+});
+}
