db673ac39c90f5883d3dcc56b5ea1ac7b0cb3104
bikanabites <bikanabites@gmail.com>
2026-06-15 21:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,17 @@
-setTimeout(()=>{
-document.getElementById("loader").style.display="none";
-},2000);
+let count = 0;
+let total = 0;
 
-let cart=[];
-let total=0;
+function addToCart(name, price){
+count++;
+total += price;
 
-function changeQty(btn,value){
-
-let qtyElement=btn.parentElement.querySelector("span");
-
-let qty=parseInt(qtyElement.innerText);
-
-qty+=value;
-
-if(qty<1) qty=1;
-
-qtyElement.innerText=qty;
+document.getElementById("count").innerText = count;
+document.getElementById("total").innerText = total;
 }
 
-function addToCart(name,price,btn){
+function orderWhatsApp(){
 
-let qty=parseInt(
-btn.parentElement.querySelector(".qty-box span").innerText
-);
+let msg = `Order from BIKANA BITES%0ATotal Items: ${count}%0ATotal: ₹${total}`;
 
-cart.push({
-name:name,
-price:price,
-qty:qty
-});
-
-total+=price*qty;
-
-document.getElementById("cartCount").innerText=cart.length;
-
-document.getElementById("cartTotal").innerText=total;
-
-alert(name+" Added To Cart");
-}
-
-function checkoutWhatsapp(){
-
-if(cart.length===0){
-
-alert("Cart Empty");
-
-return;
-}
-
-let message="🛒 BIKANA BITES Order%0A%0A";
-
-cart.forEach(item=>{
-
-message +=
-item.name+
-" x "+
-item.qty+
-" = ₹"+
-(item.price*item.qty)+
-"%0A";
-
-});
-
-message += "%0A Total = ₹"+total;
-
-window.open(
-"https://wa.me/919358500110?text="+message,
-"_blank"
-);
-}
-
-function searchProducts(){
-
-let input=document.getElementById("searchInput");
-
-let filter=input.value.toLowerCase();
-
-let cards=document.querySelectorAll(".product-card");
-
-cards.forEach(card=>{
-
-let text=card.innerText.toLowerCase();
-
-if(text.includes(filter))
-card.style.display="block";
-else
-card.style.display="none";
-
-});
+window.open("https://wa.me/919358500110?text=" + msg, "_blank");
 }
